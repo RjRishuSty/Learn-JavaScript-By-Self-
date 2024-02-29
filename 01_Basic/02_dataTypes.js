7ace5c0a2e3848a40ce25ba8b7aace0a711e8f52
Rishu Raj <117445615+rjRishuS@users.noreply.github.com>
2024-02-29 05:58:24 +0000

    Datatypes content

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+
+"use strict"; // 👉👉 use strict is nothing. but This keywoed say use all code read able or updated javaScript.
+
+
+// 😊😊😊 Datatypes in javaScripts.
+
+// JavaScript has two  categories of data types: 
+// (1) Primitive data types :-  Primitive data types are immutable. it allow only single value;
+// 😊😊 Primitive  are five datatypes.
+// (1) String :- A string is a sequence of characters
+// (2) Number :- A number is a numeric value.
+// (3) Boolean:- A boolean is a value that can be either true or false.
+// (4) Null :- A null value is a value that represents the absence of a value. Null dataTypes is Object.
+// (5) Undefined :- An undefined value is a value that has not yet been assigned.
+
+// 👉👉 How to check datatypes in Primitive.
+let result = "Hello";
+console.log(typeof result);
+
+// (2) Non Primitive dataTypes :- Non Primitive dataTypes are mutable. it allow to callection of data;
+// 😊😊 Non primitive has three datatypes.
+// (1) Array :- An array is an ordered list of values.
+// (2) Object :-  An object is a collection of properties and methods.
+// (3) Function :-  A function is a block of code that can be executed.
+
+// 👉👉 How to check datatypes in Non Primitive datatypes.
+const arr = [1,2,3,4];
+console.log(Array.isArray(arr));
