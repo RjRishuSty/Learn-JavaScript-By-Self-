f0701c4b8b375aaf9b2e70bf72e3d6d6d5c361ce
RjRishuSty <rishurajk59@gmail.com>
2024-07-23 18:38:27 +0530

    add Sync & Asynchronous

---
+++ sync & Asynchronous/index.js
@@ -0,0 +1,46 @@
+// TODO: Synchronous code........
+// TODO: what is Synchronous?
+// TODO:=> Synchronous means the code runs in a particular sequence of instructions given in the program.Each instruction waits for the previous instruction to complete its execution.
+
+// console.log("One");
+// console.log("Two");
+// console.log("Three");
+// console.log("Four");
+// console.log("Five");
+
+
+// TODO: Asynchronous...............
+// TODO: What is Asynchronous?
+// TODO:=> Due to synchronous programming, sometimes important instructions get blocked due to some previous instructions, which causes a delay in the UI. Asynchronous code execution allows to execute next instructions immediately and does not block the flow.
+
+// console.log('ONE')
+// console.log('Two')
+// setTimeout(()=>{
+//     console.log("Asynchronous code.......")
+// },2000);
+// console.log('THREE')
+// console.log('FOUR')
+
+// TODO: CallBacks...............
+// TODO: What is Callbacks?
+// TODO:=> A Callback is a function passed as a argument to another function.
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// function showAnswer(a,b,sumCallback){
+//     sumCallback(a,b);
+// }
+// showAnswer(2,2,sum) // Passed as a Argument...
+
+// TODO: Second Example...........
+// function message(){
+//     console.log('This is Callback');
+// }
+// setTimeout(message, 2000);
+
+// TODO:  Callback Hell............
+// TODO: What is callback hell?
+// TODO: => 
+
+
