83bbf38914b19953e9d94960e3984e9944dcae31
RjRishuSty <rishurajk59@gmail.com>
2024-07-24 10:00:33 +0530

    add promises

--- sync & Asynchronous/index.js
+++ sync & Asynchronous/index.js
@@ -8,7 +8,6 @@
 // console.log("Four");
 // console.log("Five");
 
-
 // TODO: Asynchronous...............
 // TODO: What is Asynchronous?
 // TODO:=> Due to synchronous programming, sometimes important instructions get blocked due to some previous instructions, which causes a delay in the UI. Asynchronous code execution allows to execute next instructions immediately and does not block the flow.
@@ -39,8 +38,44 @@
 // }
 // setTimeout(message, 2000);
 
-// TODO:  Callback Hell............
+// TODO: Callback Hell............
 // TODO: What is callback hell?
-// TODO: => 
+// TODO: => Nested callbacks stacked below one another formimg a pyramid structure.
+// TODO: Beginer Lavel.......
+// function getData(data, getNextData){
+//     setTimeout(()=>{
+//         console.log('data',data);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     },2000);
+// }
+// TODO: Callback Hell
+// getData(1,()=>{
+//     getData(2,()=>{
+//         getData(3,()=>{
+//             getData(4)
+//         })
+//     })
+// });
+
+// TODO: Call back hell ko solve karna ka laya promise ka use karta ha.
+
+//  TODO: Promises..........
+//  TODO: What is promises?
+//  TODO:=> Promise is for "eventual " completion of task. it is a object in js. it is a solution to callback hell.
+//  TODO: syntex => let promise = new promise((resolve,reject)=>{...........})
+//  TODO: promises are three stage => When a promise is created (1) Pending.
+//  TODO: (2) When promises give success status => fullfield.
+//  TODO: (3) When promise give error status => rejected.
+
+let promise = new Promise((resolve, reject) => {
+    console.log("Promises is created. it is pending status"); // TODO: pending status
+    // resolve("Data sent success") // this is fullfield status
+    reject("Get some error") // this is reject status.
+});
+
+
+
 
 
